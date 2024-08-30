0f9e5682e790489202e652c7773d198e9a33a4c8
Chandan <singhchandan98104@gmail.com>
2024-08-30 17:56:25 +0530

    navbar with mobile view

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, Link, NavLink } from "react-router-dom";
 // import { AiOutlineShareAlt, AiOutlineSearch  } from 'react-icons/ai'
@@ -8,6 +8,8 @@ import { Navbar, Typography, IconButton,Avatar, Collapse } from "@material-tailw
 const Nav = () => {
     const authStatus = useSelector((state) => state.auth.status);
     const navigate = useNavigate();
+
+    const [openNav, setOpenNav] = useState(false);
 
     // NavList
     // const navList = (
@@ -55,7 +57,8 @@ const Nav = () => {
         {
             name: "Blogs",
             slug: "/allblogs",
-            active: !authStatus,
+            active: !authStatus, 
+            //make the authStatus here true by removing "!" 
         },
         {
             name: "Admin Login",
@@ -125,12 +128,96 @@ const Nav = () => {
                         <ShareDialogBox />
                         </div>
 
+                        {/* Admin Profile Pic */}
+                        {
+                            // make the authStatus here true by removing "!". since we want that we user admin login, it show dashboard icon 
+                            !authStatus && (
+                                <div>
+                                    <Link to={'/dashboard'}>
+                                        
+                                        <div className="">
+                                            <Avatar
+                                                key={1}
+                                                src={'https://cdn-icons-png.flaticon.com/128/3135/3135715.png'}
+                                                alt="avatar"
+                                                withBorder={true}
+                                                className="p-0.5 text-red-500 w-10 h-10 hover:scale-125 duration-200"
+                                                
+                                            />
+                                        </div>
+                                    </Link>
+                                </div>
+                            )
+                        }
+
+                        {/* Mobile Toggle */}
+                        <div className="lg:hidden">
+                        <IconButton
+                                className="ml-auto h-10 w-10 border-2 border-white rounded-lg"
+                                ripple={false}
+                                onClick={() => setOpenNav(!openNav)}
+                            >
+                                {openNav ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        className="h-6 w-6"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M6 18L18 6M6 6l12 12"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                )}
+                            </IconButton>
+                        </div>
+
+                        
+
                     </div>
 
 
 
                 </div>
 
+
+                {/* Mobile View */}
+                <Collapse open={openNav}>
+                    <ul className="flex flex-col text-black w-100 gap-4">
+                        {navList.map((item) => item.active ? (
+                            <li key={item.name}>
+                                <button
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        setOpenNav(false); // Close nav on item click
+                                        navigate(item.slug);
+                                    }}
+                                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                                >
+                                    {item.name}
+                                </button>
+                            </li>
+                        ) : null)}
+                    </ul>
+                </Collapse>
             </Navbar>
         </div>
     )
