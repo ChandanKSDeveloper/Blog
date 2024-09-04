fe768cbcdd1a494af6bfc03ffb98008d3d7038e8
Chandan <singhchandan98104@gmail.com>
2024-09-04 14:11:50 +0530

    added blog skeleton

--- src/pages/allBlogs/AllBlogs.jsx
+++ src/pages/allBlogs/AllBlogs.jsx
@@ -3,7 +3,9 @@ import {BlogPostCard} from '../../components/index';
 import appwriteService from '../../appwrite/config'
 import {fetchPosts} from '../../store/allblogsSlice'
 import { useSelector, useDispatch } from "react-redux";
-
+import { Button } from "@material-tailwind/react";
+import {BlogPostSkeleton} from '../../components/index';
+import { AiOutlineLoading } from "react-icons/ai";
 
 const AllBlogs = () => {
 
@@ -11,9 +13,12 @@ const AllBlogs = () => {
     const posts = useSelector((state) => state.allblogs.posts);
     const status = useSelector((state) => state.allblogs.status);
 
+    const [visiblePosts, setVisiblePost] = useState(4);
+    const [isLoading, setIsLoading] = useState(false); // State to track loading
+
     useEffect(() => {
         if(status === 'idle'){
-            dispatch(fetchPosts());
+            dispatch(fetchPosts()).then(() => setIsLoading(false));
         }
     },[dispatch,status])
 
@@ -22,24 +27,77 @@ const AllBlogs = () => {
     },[fetchPosts])
 
 
-    // const [posts,setPosts] = useState([]);
-    // useEffect(() => {
-    //     appwriteService.getPosts([]).then((post) => {
-    //         if(post){
-    //             setPosts(post.documents);
-    //         }
-    //     })
-    // },[])
+    const loadMorePosts = () => {
+        setIsLoading(true);
+        setVisiblePost((prev) => prev + 4);
+        setIsLoading(false);
+    }
 
     return(
-        <div className='w-full py-8'>
-            <div className='flex flex-wrap grow'>
-                {posts.map((post) => (
+        <div className='w-full py-8 flex-col items-center justify-center'>
+
+            <div className='flex flex-wrap grow items-center justify-center'>
+
+                {
+                    status === 'loading' && (
+                        // Render skeleton placeholders while posts are loading
+                        <>
+                            <BlogPostSkeleton />
+                            <BlogPostSkeleton />
+                            <BlogPostSkeleton />
+                            <BlogPostSkeleton />
+                        </>
+                    )
+                }
+
+                {
+                    posts.slice(0, visiblePosts).map((post) => (
+                        <div key={post.$id} className='p-2'>
+                            <BlogPostCard {...post} />
+                        </div>
+                    ))
+                }
+
+                </div>
+
+                {
+                    visiblePosts < posts.length && (
+                        <div className="flex justify-center pb-5">
+                            <Button
+                                onClick={loadMorePosts}
+                                disabled = {isLoading}>
+                                {
+                                    isLoading ? <AiOutlineLoading className="animate-spin"/> : "Load More"
+                                }
+
+                            </Button>
+
+                        </div>
+                    )
+                }
+
+                {/* Button gets disabled and hidden if no more posts to load */}
+                {
+                    visiblePosts >= posts.length && (
+                        <div className="flex justify-center pb-5">
+                        <button
+                            className="bg-gray-500 text-white py-2  px-4 rounded mt-4 cursor-not-allowed opacity-50"
+                            disabled
+                        > No more Posts
+
+                        </button>
+
+                        </div>
+                    )
+                }
+
+
+                {/* {posts.map((post) => (
                     <div key={post.$id} className='p-2 w-full sm:w-1/2 lg:w1/4'>
                         <BlogPostCard {...post} />
                     </div>
-                ))}
-            </div>
+                ))} */}
+            
             
     </div>
     )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
-import React,{useEffect} from "react";
+import React,{useEffect, useState} from "react";
 import authService from '../../appwrite/auth'
-import {HeroSection, BlogPostCard,Layout} from '../../components/index'
+import { HeroSection, BlogPostCard, Layout, BlogPostSkeleton } from '../../components/index'
 import { useSelector,useDispatch } from "react-redux";
 import toast from "react-hot-toast";
 
+import {fetchPosts} from '../../store/allblogsSlice';
+import { Button } from "@material-tailwind/react";
 
 const Home = () => {
+
     const isLoggedIn = useSelector((state) => state.auth.status);
     const userData = useSelector((state) => state.auth.userData);
     console.log(isLoggedIn);
@@ -14,6 +17,25 @@ const Home = () => {
     const dispatch = useDispatch();
 
 
+    const [visiblePosts, setVisiblePost] = useState(4); // Number of posts to show initially
+    const [isLoading, setIsLoading] = useState(false); // State to track loading
+    
+    const posts = useSelector((state) => state.allblogs.posts);
+    const status = useSelector((state) => state.allblogs.status);
+
+
+    useEffect(() => {
+        if(status === 'idle'){
+            setIsLoading(true); // Start Loading
+            // dispatch(fetchPosts());
+            dispatch(fetchPosts()).then(() => setIsLoading(false)); // Stop loading after fetching
+        }
+    },[dispatch,status])
+
+    useEffect(() => {
+        fetchPosts();
+    },[fetchPosts])
+
     useEffect(() => {
         const checkSession = async() => {
             try{
@@ -21,18 +43,93 @@ const Home = () => {
                 if(user){
                     dispatch(authLogin(user));
                     toast.success("Welcome back")
-                    // navigate('/')
+                } else {
+                    console.error("No user session found.");
                 }
             } catch(error) {
                 // No active session or error fetching user
+                console.error("No active session or error fetching user:", error);
             }
         };
         checkSession();
     },[dispatch])
+
+
+    // Function to load more Posts
+    const loadMorePosts = () => {
+        setIsLoading(true); // Start loading
+        setVisiblePost((prev) => prev + 4);// Increase the number of visible posts by 4
+        setIsLoading(false);  // Stop loading after posts are added
+    } 
+
     return(
         <Layout>
             <HeroSection />
-            <BlogPostCard />
+
+
+               <div className="bg-[#FFEEAD]">
+
+
+            <div className=" flex flex-wrap justify-center items-center bg-[#FFEEAD]">
+
+            {
+                status === 'loading' && (
+                     // Render skeleton placeholders while posts are loading
+                     <>
+                        <BlogPostSkeleton />
+                        <BlogPostSkeleton />
+                        <BlogPostSkeleton />
+                        <BlogPostSkeleton />
+                    </>
+                )
+            }
+
+            {/* {
+                posts.map((post) => (
+                    <div key={post.$id} className='p-2'>
+                        <BlogPostCard {...post} />
+                    </div>
+                ))
+            } */}
+
+            {
+                posts.slice(0, visiblePosts).map((post) => (
+                    <div key={post.$id} className='p-2'>
+                        <BlogPostCard {...post} />
+                    </div>
+                ))
+            }
+            </div>
+
+            {
+                visiblePosts < posts.length && (
+                    <div className="flex justify-center pb-5">
+                        <Button
+                            onClick={loadMorePosts}
+                            disabled={isLoading}
+                            >
+                           {isLoading ? "Loading..." : "Load More"}
+                        </Button>
+                    </div>
+                )
+            }
+            
+            {/* Button gets disabled and hidden if no more posts to load */}
+            {
+                visiblePosts >= posts.length && (
+                    <div className="flex justify-center pb-5">
+                    <button
+                        className="bg-gray-500 text-white py-2  px-4 rounded mt-4 cursor-not-allowed opacity-50"
+                        disabled
+                    > No more Posts
+
+                    </button>
+
+                    </div>
+                )
+            }
+
+            </div>
 
         </Layout>
     )
