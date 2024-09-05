1c79744c1b05394789c85f307f817c6bf8437bcf
Chandan <singhchandan98104@gmail.com>
2024-09-05 18:23:46 +0530

    rte

--- src/components/RTE/RTE.jsx
+++ src/components/RTE/RTE.jsx
@@ -44,7 +44,8 @@ const RTE = ({name, control, label, defaultValue = ""}) => {
                             ],
                             toolbar:
                             "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-                            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+                            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                            readonly: false,
                         }}
                         onEditorChange={onChange}
                         />
